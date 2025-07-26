bd6d465673a090482a75257255293ecdba560a48
Jhon Mark Ruiz Arnaiz <jhon.arnaiz@jeonsoft.com>
2025-07-26 18:21:00 +0800

    ARCHITECTURE: add routes for technologies,certificates and experience components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,17 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { TechnologiesComponent } from './pages/technologies/technologies.component';
+import { CertificatesComponent } from './pages/certificates/certificates.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'projects', component: ProjectsComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'technologies', component: TechnologiesComponent },
+  { path: 'certificates', component: CertificatesComponent },
   { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '' },
 ];
