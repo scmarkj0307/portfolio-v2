f297a64d7173b55cd858b7bd63320402fe9e575c
Jhon Mark Ruiz Arnaiz <jhon.arnaiz@jeonsoft.com>
2025-07-26 20:36:24 +0800

    UI ENHANCEMENT: add content on project component and disable hover on cards

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -2,31 +2,54 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { SharedService } from '../../shared.service';
 import { Subscription } from 'rxjs';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent implements OnInit, OnDestroy {
+  backgroundState = 2 
    private subscription!: Subscription;
-
-  constructor(
-    private router: Router,
-    private sharedService: SharedService
-  ) {}
-
-  ngOnInit(): void {
-    this.subscription = this.sharedService.homeState$.subscribe(state => {
-      if (state === 2) {
-        this.router.navigate(['/']);
-      }
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
+   private destroy$ = new Subject<void>();
+   
+ 
+   constructor(
+     private router: Router,
+     private sharedService: SharedService
+   ) {}
+ 
+   ngOnInit(): void {
+     this.subscription = this.sharedService.homeState$.subscribe(state => {
+       if (state === 2) {
+         this.router.navigate(['/']);
+       }
+     });
+ 
+      this.sharedService.backgroundState$
+           .pipe(takeUntil(this.destroy$))
+           .subscribe((state) => {
+             this.backgroundState = state;
+           });
+   }
+ 
+   ngOnDestroy(): void {
+     this.subscription.unsubscribe();
+       this.destroy$.next();
+     this.destroy$.complete();
+   }
+ 
+    getBackgroundClass(): string {
+     switch (this.backgroundState) {
+       case 0: return 'cosmos-background';
+       case 1: return 'plain-white-background';
+       default: return 'image-background';
+     }
+   }
 }
