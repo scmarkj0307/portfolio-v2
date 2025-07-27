0b4dc31f75dd2302ad14780a8ba3fb6503cf01e5
Jhon Mark Ruiz Arnaiz <jhon.arnaiz@jeonsoft.com>
2025-07-27 15:35:57 +0800

    UI ENHANCMENT: support email sender to my gmail

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -5,11 +5,16 @@ import { Subscription } from 'rxjs';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import { MatSnackBar, MatSnackBarModule  } from '@angular/material/snack-bar';
+
+
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,MatSnackBarModule ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
@@ -21,7 +26,8 @@ export class ContactComponent implements OnInit, OnDestroy {
 
   constructor(
     private router: Router,
-    private sharedService: SharedService
+    private sharedService: SharedService,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
@@ -51,4 +57,30 @@ export class ContactComponent implements OnInit, OnDestroy {
       default: return 'image-background';
     }
   }
+
+   lastEmailTimestamp: number | null = null;  // store the last sent time
+cooldownMinutes = 5;
+
+public sendEmail(e: Event): void {
+  e.preventDefault();
+
+  const now = Date.now();
+
+  if (this.lastEmailTimestamp && (now - this.lastEmailTimestamp) < this.cooldownMinutes * 60 * 1000) {
+    const remaining = Math.ceil(((this.cooldownMinutes * 60 * 1000 - (now - this.lastEmailTimestamp)) / 1000) / 60);
+    this.snackBar.open(`Please wait ${remaining} more minute(s) before sending again.`, 'Close', { duration: 4000,  verticalPosition: 'top' });
+    return;
+  }
+
+  emailjs.sendForm('service_l7cyhsi', 'template_r8k23lz', e.target as HTMLFormElement, '5JtO2FAppmBbugUNj')
+    .then((result: EmailJSResponseStatus) => {
+      this.lastEmailTimestamp = now;  // record the time of successful send
+      this.snackBar.open('Message sent successfully!', 'Close', { duration: 3000,  verticalPosition: 'top' });
+      (e.target as HTMLFormElement).reset(); // optional: clear form
+    }, (error) => {
+      this.snackBar.open('Failed to send message. Please try again later.', 'Close', { duration: 4000,  verticalPosition: 'top' });
+      console.error(error.text);
+    });
+}
+
 }
